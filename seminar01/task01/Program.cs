42c5d4f92dd991a3e6e0f9c3f49e7390874c472a
evgenii <matazov2009@gmail.com>
2023-08-01 23:03:22 +0300

    semminar01

---
+++ seminar01/task01/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите число, которое хотите сделать квадратным");
+string input = Console.ReadLine();
+int number = Convert.ToInt32(input);
+result = number * number;
+Console.WriteLine(result);
---
+++ seminar01/task04/Program.cs
@@ -0,0 +1,9 @@
+﻿int number = Convert.ToInt32(Console.ReadLine());
+int number2 = -number;
+int index = number2;
+while(index <= number)
+{
+    Console.Write(" " + number2);
+    number2 ++;
+    index ++;
+}
