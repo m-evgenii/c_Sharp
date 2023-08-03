9e1234d2abb999054f5cb20a31c88b564abb4a9d
evgenii <matazov2009@gmail.com>
2023-08-03 13:35:46 +0300

    leckson02

---
+++ lekshon02/example01_function/Program.cs
@@ -0,0 +1,18 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2>result) result = arg2;
+    if(arg3>result) result = arg3;
+    return result;
+}
+int a1 = 5;
+int b1 = 35;
+int c1 = 8;
+int a2 = 6;
+int b2 = 25;
+int c2 = 87;
+int a3 = 45;
+int b3 = 644;
+int c3 = 18;
+int max = Max(Max(a1,b1,c1),Max(a2,b2,c2),Max(a3,b3,c3));
+Console.WriteLine(max);
---
+++ lekshon02/example02_aray/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] array = {4,5,48,3,45,2,6,9};
+int find = 9;
+int result = -1;
+int index = 0;
+while(index<array.Length)
+{
+    if(array[index] == find){
+        result = index;
+    }
+    index++;
+}
+Console.WriteLine(result);
