ba5b53466b9a544f762876ba18d51045b4113a98
evgenii <matazov2009@gmail.com>
2023-09-08 13:50:09 +0300

    seminar06_homework

---
+++ seminar06/homework/task01/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите кол-во чисел в массиве");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[m];
+Console.WriteLine("Введите числа");
+void FillArray(int[] array){
+    for(int i = 0; i < m; i ++){
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    Console.WriteLine(String.Join(" ", array));
+}
+FillArray(array);
+int count = 0;
+int Counter(int[] array){
+    for(int i = 0; i < array.Length; i ++){
+        if (array[i] > 0){
+            count ++;
+        }
+    }
+    return count;
+}
+Counter(array);
+Console.WriteLine(count);
---
+++ seminar06/homework/task02/Program.cs
@@ -0,0 +1,7 @@
+﻿double b1 = Convert.ToInt32(Console.ReadLine());
+double k1 = Convert.ToInt32(Console.ReadLine());
+double b2 = Convert.ToInt32(Console.ReadLine());
+double k2 = Convert.ToInt32(Console.ReadLine());
+double x =(b2 - b1) / (k1 - k2);
+double y=k1*x+b1;
+Console.WriteLine(x + "; " + y);
