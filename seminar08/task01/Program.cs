3fbda9dcb128e1162e2c253844152ef1b7a5113a
evgenii <matazov2009@gmail.com>
2023-09-08 21:22:59 +0300

    seminar08

---
+++ seminar08/task01/Program.cs
@@ -0,0 +1,43 @@
+﻿int[,] array = new int[3, 4];
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+FillArray(array);
+int[,] array2 = new int[3, 4];
+void ChangeArray(int[,] array){
+    for(int i2 = array.GetLength(1); i2 > 0; i2 --){
+        for (int i = 0; i < array2.GetLength(1); i ++){
+        array2[0, i] = array[array.GetLength(0), i2];
+        }
+    }
+}
+ChangeArray(array);
+Console.WriteLine("-----------------------");
+void Vivod(int[,] array2){
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array2[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+Vivod(array2);
+//    int res = 0;
+//    for (int i = 0; i < array.GetLength(1); i++)
+//    {
+//        res = array[0, i];
+//        array[0, i] = array[array.GetLength(0) - 1, i];
+//        array[array.GetLength(0) - 1, i] = res;
+//}
---
+++ seminar08/task02/Program.cs
@@ -0,0 +1,34 @@
+﻿int m = 4;
+int n = 4;
+int[,] array = new int[m, n];
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+FillArray(array);
+Console.WriteLine("-----------------------");
+int[,] array2 = new int[n, m];
+void FinalArray(int[,] array2, int[,] array)
+{
+    for (int i = 0; i < array2.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2.GetLength(1); j++)
+        {
+            array2[j, i] = array[j, i];
+            Console.Write(array2[j, i] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+FinalArray(array2, array);
+if(m != n){
+    Console.WriteLine("Невозможно перевернуть массив!");
+}
