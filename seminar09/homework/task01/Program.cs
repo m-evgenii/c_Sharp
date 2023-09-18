f9fb732f41663e1b9917a7f53f95859a2617ebd6
evgenii <matazov2009@gmail.com>
2023-09-18 18:18:33 +0300

    seminar09homework

---
+++ seminar09/homework/task01/Program.cs
@@ -0,0 +1,11 @@
+﻿int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+int Sum(int m, int n){
+    if(m == n + 1){
+        return sum;
+    }
+    return sum = m + Sum(m + 1, n);
+}
+Sum(m,n);
+Console.WriteLine(sum);
---
+++ seminar09/homework/task02/Program.cs
@@ -0,0 +1,13 @@
+﻿int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int ack(int m, int n){
+    if(m == 0){
+        return n + 1;
+    }
+    else if(n == 0){
+        return ack(m - 1, 1);
+    }
+    return ack(m-1, ack (m, n - 1));
+}
+int final = ack(m,n);
+Console.WriteLine(final); //первое задание вы сказали можно не делать
